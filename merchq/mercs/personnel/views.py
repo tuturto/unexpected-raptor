0dc641c4d046305dcb7ace7d9504fe6aad52a8db
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-08-23 06:27:34 +0300

    working on personnel

--- merchq/mercs/personnel/views.py
+++ merchq/mercs/personnel/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from django.db.models import Count
+from django.shortcuts import render
+
+from mercs.personnel.models import Person
+from mercs.forces.models import Force
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the personnel index.")
+    forces = Force.objects.all().annotate(personnel_count = Count('person'))
+    context = {'forces': forces}
+
+    return render(request, 'personnel/index.html', context)
 
 def force_personnel(request, force_id):
     return HttpResponse("Hello, world. You're seeing a force")
+
+def person_details(request, person_id):
+	return HttpResponse("Hello, world. You're seeing a person")
+
