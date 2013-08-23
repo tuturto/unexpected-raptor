4eaf658ec8360bef452d78bb4e63041a9c3e151e
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-08-23 07:07:14 +0300

    fixed indentation

--- merchq/mercs/personnel/urls.py
+++ merchq/mercs/personnel/urls.py
@@ -5,6 +5,6 @@ from mercs.personnel import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<force_id>\d+)/$', views.force_personnel, name='force_personnel'),
-	url(r'^(?P<force_id>\d+)/$', views.force_personnel, name='force_personnel'),
-	url(r'^person/(?P<person_id>\d+)/$', views.person_details, name='person_details'),
+    url(r'^(?P<force_id>\d+)/$', views.force_personnel, name='force_personnel'),
+    url(r'^person/(?P<person_id>\d+)/$', views.person_details, name='person_details'),
 )
