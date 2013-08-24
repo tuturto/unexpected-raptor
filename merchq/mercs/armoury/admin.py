009839e29538fb0f2cd96e45f726942d3f2dcc96
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-08-24 10:53:36 +0300

    working on armoury

--- merchq/mercs/armoury/admin.py
+++ merchq/mercs/armoury/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from mercs.armoury.models import VehicleType, WeightClass, SupportType
+from mercs.armoury.models import Vehicle
+
+admin.site.register(VehicleType)
+admin.site.register(WeightClass)
+admin.site.register(SupportType)
+admin.site.register(Vehicle)
--- merchq/mercs/armoury/models.py
+++ merchq/mercs/armoury/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from mercs.forces.models import Force
 
-# Create your models here.
+class VehicleType(models.Model):
+    type_name = models.CharField(max_length = 50)
+    
+    def __unicode__(self):
+        return self.type_name
+
+class WeightClass(models.Model):
+    weight_class = models.CharField(max_length = 50)
+    vehicle_type = models.ForeignKey(VehicleType)
+    lower_limit = models.IntegerField(default = 0)
+    upper_limit = models.IntegerField(default = 0)
+    
+    def __unicode__(self):
+        return '{0} {1} ({2} - {3})'.format(self.weight_class,
+                                            self.vehicle_type,
+                                            self.lower_limit,
+                                            self.upper_limit)
+
+class SupportType(models.Model):
+    support_name = models.CharField(max_length = 50)
+
+    def __unicode__(self):
+        return self.support_name
+
+class Vehicle(models.Model):
+    vehicle_name = models.CharField(max_length = 50)
+    vehicle_type = models.ForeignKey(VehicleType)
+    vehicle_weight = models.IntegerField(default = 0)
+
+    weekly_maintenance_cost = models.IntegerField(default = 0)
+    weekly_support = models.FloatField(default = 0)
+    support_type = models.ForeignKey(SupportType)
+
+    owner = models.ForeignKey(Force)
+
+    def __unicode__(self):
+        return '{0} ({1})'.format(self.vehicle_name,
+                                  self.id)
+
