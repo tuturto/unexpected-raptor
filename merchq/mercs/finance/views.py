0ffa6b266b23358ba70c9ee4cc107fa52294973a
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-08-25 22:57:21 +0300

    working on finances

--- merchq/mercs/finance/views.py
+++ merchq/mercs/finance/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from mercs.forces.models import Force
+
+def index(request):
+    forces = Force.objects.all()
+
+    context = {}
+
+    return render(request, 'finances/index.html', context)
+
+def force_finances(request, force_id):
+    forces = Force.objects.all()
+
+    context = {}
+
+    return render(request, 'finances/force_finances.html', context)
+
