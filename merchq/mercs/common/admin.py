c59f3b35d5dd912d401fc75f879f9b13342d1a56
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-08-28 20:22:00 +0300

    updated license notification

--- merchq/mercs/common/admin.py
+++ merchq/mercs/common/admin.py
@@ -2,20 +2,20 @@
 
 #   Copyright 2013 Tuukka Turto
 #
-#   This file is part of mercs.
+#   This file is part of unexpected-raptor.
 #
-#   mercs is free software: you can redistribute it and/or modify
+#   unexpected-raptor is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
-#   mercs is distributed in the hope that it will be useful,
+#   unexpected-raptor is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with pyherc.  If not, see <http://www.gnu.org/licenses/>.
+#   along with unexpected-raptor.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.contrib import admin
 from mercs.common.models import Parameter
