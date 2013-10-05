cc8877baf704d1420cfd304bf3ed755fbd0290db
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-10-06 00:38:00 +0300

    working on displaying current date

--- merchq/merc_client/web/merc_client.dart
+++ merchq/merc_client/web/merc_client.dart
@@ -1,18 +1,18 @@
 library merc_client;
 
 import 'dart:html';
+import 'dart:json';
+import 'dart:async';
+import 'package:dart_config/default_browser.dart';
+
+DateTime currentDate = null;
+DateTime shownDate = null;
+Map config = null;
 
 void main() {
-  query("#sample_text_id")
-    ..text = "Click me!"
-    ..onClick.listen(reverseText);
-}
-
-void reverseText(MouseEvent event) {
-  var text = query("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  query("#sample_text_id").text = buffer.toString();
+  
+  loadConfig('/static/config.yaml')
+    .catchError((e) => loadConfig()).then((Map c) {
+      config = c;
+  });
 }
