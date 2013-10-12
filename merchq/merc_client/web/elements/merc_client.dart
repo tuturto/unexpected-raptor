80f32cfb22b41a2a7c90420bad2082f9d2492d80
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-10-12 16:15:09 +0300

    dynamically create app menu

--- merchq/merc_client/web/elements/merc_client.dart
+++ merchq/merc_client/web/elements/merc_client.dart
@@ -12,12 +12,23 @@ class MercClientElement extends PolymerElement {
   void startUp(Map config) {
     this.config = config;
 
+    show_gm_application();
+  }
+  
+  void show_gm_application() {
+    shadowRoot.query('#app_menu1').setInnerHtml('Logs');
+    shadowRoot.query('#app_menu2').setInnerHtml('Cycle');
+    
+    show_gm_logs();
+  }
+  
+  void show_gm_logs() {
     var dateElement = createElement('current-date');
     shadowRoot.query('#navbar').children.add(dateElement);
     dateElement.xtag.startUp(config);
     
     var gmLogElement = createElement('gm-log');
     shadowRoot.query('#appcontent').children.add(gmLogElement);
-    gmLogElement.xtag.startUp(config);
+    gmLogElement.xtag.startUp(config);    
   }
 }
